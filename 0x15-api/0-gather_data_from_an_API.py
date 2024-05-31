95cdf14e1327ac08ce5bb525615d66e50f673abf
bg-miles <piuschukwuebuka40@gmail.com>
2024-05-31 01:08:28 +0100

    todo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,24 +6,25 @@ import json
 import requests
 import sys
 
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
 
-user_id = int(sys.argv[1])
-user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    # Getting the username
+    user_response = requests.get(user_url)
+    if user_response.status_code == 200:
+        users = user_response.json()
+        user_name = users["name"]
 
-# Getting the username
-user_response = requests.get(user_url)
-if user_response.status_code == 200:
-    users = user_response.json()
-    user_name = users["name"]
+    # Getting the todos
+    todo_reponse = requests.get(url)
+    if todo_reponse.status_code == 200:
+        todos = todo_reponse.json()
+        completed_task = [todo for todo in todos if todo["completed"]]
 
-# Getting the todos
-todo_reponse = requests.get(url)
-if todo_reponse.status_code == 200:
-    todos = todo_reponse.json()
-    completed_task = [todo for todo in todos if todo["completed"]]
-
-
-print(f"Employee {user_name} is done with tasks({len(completed_task)}/{len(todos)}):")
-for tasks in completed_task:
-    print(f"\t{tasks['title']}")
+    print(
+        f"Employee {user_name} is done with tasks({len(completed_task)}/{len(todos)}):"
+    )
+    for tasks in completed_task:
+        print(f"\t{tasks['title']}")
