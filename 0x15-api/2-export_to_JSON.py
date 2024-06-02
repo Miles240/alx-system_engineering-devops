814705bee335007d64225e357ab8d30d62961cc6
bg-miles <piuschukwuebuka40@gmail.com>
2024-06-03 00:36:55 +0100

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,3 +1,31 @@
-with open('hog.csv', 'r', encoding='utf-8') as file:
-	for line in file:
-		print(line)
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    user_id = sys.argv[1]
+    filename = f"{user_id}.json"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+
+    user = requests.get(user_url).json()
+    todos = requests.get(todo_url).json()
+
+    with open(filename, "w", encoding="utf-8") as jsonfile:
+        json.dump(
+            {
+                user_id: [
+                    {
+                        "task": t.get("title"),
+                        "completed": t.get("completed"),
+                        "username": user.get("username"),
+                    }
+                    for t in todos
+                ]
+            },
+            jsonfile,
+        )
