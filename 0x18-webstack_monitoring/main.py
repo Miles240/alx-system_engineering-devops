8533eead923dc260c75059a2a542114424269b03
bg-miles <piuschukwuebuka40@gmail.com>
2024-06-11 15:23:55 +0100

    venv

--- 0x18-webstack_monitoring/main.py
+++ 0x18-webstack_monitoring/main.py
@@ -1,13 +1,18 @@
-import praw
-
-# Create a Reddit instance
-reddit = praw.Reddit(
-    client_id="nlOpORtYC8UH7JblzSFYeA",
-    client_secret="NgmFtp-5Sg4MgFsvlcqesowvVOalQQ",
-    user_agent="Miles/0.0.1 by bg_miles",
-)
-
-# Get the top 10 hot posts from the Python subreddit
-subreddit = reddit.subreddit("Python")
-for submission in subreddit.hot(limit=10):
-    print(f"Title: {submission.title}, Upvotes: {submission.score}")
+{
+    "id": 146770375,
+    "name": "High Read Requests on Device",
+    "type": "query alert",
+    "query": "avg(last_5m):avg:system.load.1{host:429651-web-01} by {host} > 100",
+    "message": "<!--StartFragment-->\n\nHigh Read Requests on Device\n\n<!--EndFragment-->",
+    "tags": [],
+    "options": {
+        "thresholds": {"critical": 100},
+        "notify_audit": false,
+        "include_tags": true,
+        "new_group_delay": 60,
+        "notify_no_data": false,
+        "silenced": {},
+    },
+    "priority": null,
+    "restricted_roles": null,
+}
