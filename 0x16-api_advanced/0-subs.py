8d59d3710042e5338db4a0b004872ffccc81e802
bg-miles <piuschukwuebuka40@gmail.com>
2024-06-14 13:31:28 +0100

    json

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,12 @@ def number_of_subscribers(subreddit):
     """function that queries the Reddit API
     and returns the number of subscribers
     """
-    url = f"https://oauth.reddit.com/r/{subreddit}/about"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Custom"}
 
     res = requests.get(url, headers, allow_redirects=False)
 
     if res.status_code == 200:
         return res.json().get("data").get("subscribers")
-    return 0
+    else:
+        return 0
